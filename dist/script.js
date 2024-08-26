32bdd129d2765f3e16f607be67dd08e52494056b
Harshdeep Singh <harshdeep.singh@IN-CXLPMAC153.local>
2024-08-26 22:01:20 +0530

    initial commit

---
+++ dist/script.js
@@ -0,0 +1,18 @@
+const navDialog = document.getElementById('nav-dailog');
+function handleMenu(){
+    navDialog.classList.toggle('hidden');
+}
+
+
+// const dtElements =document.querySelectorAll('dt');
+// dtElements.forEach(element =>{
+//     element.addEventListener('click', () =>{
+//         const ddId = element.getAttribute('aria-controls');
+//         const ddElement =document.querySelectorAll('img')[0];
+
+//         ddElement.classList.toggle('hidden');
+//         ddArrowIcon.classList.toggle(-rotate-180);
+//     }
+
+//     )
+// })
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {},
+    fontFamily:{
+      'display':['Poppins' ,'sans-serif'],
+      'body':['Inter','sans-serif']
+    }
+  },
+  plugins: [],
+}
+
